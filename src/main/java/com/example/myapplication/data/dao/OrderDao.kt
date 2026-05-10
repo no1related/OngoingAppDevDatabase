2e933bb6c9efab31090ec0e22c00d9332e991294
no1related <100349932+no1related@users.noreply.github.com>
2026-05-10 10:41:29 +0800

    Delete src directory

--- src/main/java/com/example/myapplication/data/dao/OrderDao.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication.data.dao
-
-interface OrderDao {
-}
--- src/main/java/com/example/myapplication/ui/user/UserSortType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplication.ui.user
-
-enum class UserSortType {
-    USERNAME,
-    EMAIL,
-    CREATED_AT
-}
